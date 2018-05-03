d7728a38e85eb643fb342fef04b8353e88c4f703
Nick Ye <nick.ye888@gmail.com>
2018-05-03 21:54:46 +1200

    bot can repeat user input

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 import * as builder from 'botbuilder';
+import * as restify from 'restify';
 
-let connector = new builder.ConsoleConnector().listen();
-let bot = new builder.UniversalBot(connector, ((session) => {
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3000, () => {
+  console.log('%s listening to %s', server.name, server.url);
+});
+
+const connector = new builder.ChatConnector({
+  appId: process.env.MicrosoftAppId,
+  appPassword: process.env.MicrosoftAppPassword,
+});
+
+server.post('/api/messages', connector.listen());
+
+const bot = new builder.UniversalBot(connector, ((session) => {
   session.send('You said: %s', session.message.text);
 }));
